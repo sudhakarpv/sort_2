a13f19fc262055c3168efbfa9ce91a03c081e276
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-21 10:06:13 +0530

    Update sort_2.py

--- sort_2.py
+++ sort_2.py
@@ -1,14 +1,13 @@
 # sort_2
 def main():
     pass
-def smallnum(n):
+    l=input()
+    n=list(map(int,input().split()))
     for i in range(len(n)):
         for k in range (i+1,len(n)):
             if n[k]<n[i]:
                 n[i],n[k]=n[k],n[i]
     print(' '.join(map(str,n)))
-
-smallnum (n=list(map(int,input().split())))
 
 if __name__ == '__main__':
     main()
